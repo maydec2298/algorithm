bb86e525b3ce56d13b82e1c297309ab5a3a98924
Mina Yoo <maydec2298@gmail.com>
2022-08-22 10:20:21 +0900

    2022.08.22

--- 두 개 뽑아서 더하기.js
+++ 두 개 뽑아서 더하기.js
@@ -1,5 +1,20 @@
-function solution(numbers) {
+let answer = [];
 
+
+function solution(numbers) {
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      sum = numbers[i] + numbers[j]
+      if (answer.indexOf(sum) === -1) {
+        answer.push(sum);
+      };
+    }
+  }
+
+  return answer.sort((a, b) => a - b)
 };
 
+
+
 console.log(solution([2, 1, 3, 4, 1]));
+
